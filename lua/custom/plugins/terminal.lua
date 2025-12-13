627ff0e3b7a9694d6629d597a2d7d86849a9f7a1
noahapursell <noahapursell@gmail.com>
2025-12-12 19:40:53 -0600

    added jj to be escape for terminal

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local term_buf = nil
+
 vim.api.nvim_create_autocmd('TermOpen', {
   group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
   callback = function()
@@ -8,15 +10,25 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
-vim.keymap.set('n', '<leader>st', function()
-  if vim.bo.buftype == 'terminal' then
-    vim.cmd 'close'
-    return
-  end
+vim.keymap.set('n', '<leader>tt', function()
+  if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_get_buf(win) == term_buf then
+        vim.api.nvim_win_close(win, true)
+        return
+      end
+    end
 
-  vim.cmd 'botright split | term'
+    vim.cmd 'botright split'
+    vim.api.nvim_win_set_buf(0, term_buf)
+  else
+    vim.cmd 'botright split | term'
+    term_buf = vim.api.nvim_get_current_buf()
+  end
   vim.cmd 'wincmd J'
   pcall(vim.api.nvim_win_set_height, 0, 15)
-end, { desc = 'Open Small Terminal' })
+end, { desc = 'Toggle Terminal' })
+
+vim.keymap.set('t', 'jj', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
 
 return M

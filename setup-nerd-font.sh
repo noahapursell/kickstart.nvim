b46e7fc0f6b4521f91b3861aa198924653fc379b
noahapursell <noahapursell@gmail.com>
2026-03-10 08:19:36 -0500

    updated nerd fonts to also download codeNewRoman font

--- setup-nerd-font.sh
+++ setup-nerd-font.sh
@@ -1,1 +1,3 @@
 wget -P ~/.fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip && cd ~/.fonts && unzip JetBrainsMono.zip && rm JetBrainsMono.zip && fc-cache -fv
+wget -P ~/.fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CodeNewRoman.zip && cd ~/.fonts && unzip CodeNewRoman.zip && rm CodeNewRoman.zip && fc-cache -fv
+
